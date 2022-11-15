12ceca287e2eee01d8620139a11d83f364cd2e2a
Abdeslam Loukili <abdeslam.edu@gmail.com>
2022-11-15 19:35:41 +0100

    participant service and dao impl

--- src/main/java/org/loukili/javac/service/ParticipantService.java
+++ src/main/java/org/loukili/javac/service/ParticipantService.java
@@ -1,32 +1,38 @@
 package org.loukili.javac.service;
 
+import org.loukili.javac.dao.ActivityDao;
+import org.loukili.javac.dao.Idao;
+import org.loukili.javac.dao.ParticipantDao;
+import org.loukili.javac.entity.Activity;
 import org.loukili.javac.entity.Participant;
 
 import java.util.List;
 
 public class ParticipantService implements ICRUDService<Participant> {
+
+  private Idao<Participant> participantDao = new ParticipantDao();
   @Override
   public Participant find(long id) {
-    return null;
+    return participantDao.find(id);
   }
 
   @Override
   public List<Participant> getAll() {
-    return null;
+    return participantDao.getAll();
   }
 
   @Override
-  public Participant add(Participant user) {
-    return null;
+  public Participant add(Participant participant) {
+    return participantDao.add(participant);
   }
 
   @Override
-  public void update(Participant user) {
-
+  public void update(Participant participant) {
+    participantDao.add(participant);
   }
 
   @Override
   public boolean delete(long id) {
-    return false;
+    return participantDao.delete(id);
   }
 }
