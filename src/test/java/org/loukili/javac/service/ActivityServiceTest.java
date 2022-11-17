77b9077c04432ffc487a5640e653ce3aa8d7ef27
Abdeslam Loukili <abdeslam.edu@gmail.com>
2022-11-17 10:59:41 +0100

    ready

---
+++ src/test/java/org/loukili/javac/service/ActivityServiceTest.java
@@ -0,0 +1,13 @@
+package org.loukili.javac.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivityServiceTest {
+
+  @Test
+  void shouldGetAllEntities() {
+
+  }
+}
